4a71c0841be89e8fa692144b5011d70f16785355
vaisinghequinix <vaisingh@equinix.com>
2023-03-09 08:29:36 +0530

    session 5

---
+++ src/te_march_morning/FirstEclipseSession.java
@@ -0,0 +1,12 @@
+package te_march_morning;
+
+public class FirstEclipseSession {
+	public static void main(String[] args) {
+		System.out.println("Hello this is first eclipse class");
+	}
+}
+
+// Blocking language
+// For execution - main
+// main- Ctrl+Space
+// sysout - ctrl+space
---
+++ src/te_march_morning/Test12.java
@@ -0,0 +1,23 @@
+package te_march_morning;
+
+public class Test12 {
+	// first letter must be alphabet,_,$
+	// should - mynameiskhan = MyNameIsKhan
+	// earthisagreatplanet = EarthIsAGreatPlanet
+	// PLural - never make 
+	// Student - allowed , Students - not allowed
+	public static void main(String[] args) {
+		//	keyword = in compiler, predefined meaning 
+		// package, public , class, static, void
+		
+		// variable = vary(which can change) + able
+		int i = 10;
+		i = 12;
+		
+		// constant = final keyword
+		
+//		Rule - will never same as keyword 
+		int k = 78;
+		
+	}
+}
