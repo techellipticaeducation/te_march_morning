d420c21aa40c362d4bd6c86ff0a37dbabed9e615
vaisinghequinix <vaisingh@equinix.com>
2023-03-13 08:13:32 +0530

    data

---
+++ src/te_march_morning/DataTypeDemo.java
@@ -0,0 +1,90 @@
+package te_march_morning;
+
+public class DataTypeDemo {
+	public static void main(String[] args) {
+		 //	DataType = Data + Type
+		
+		 // numeric data = 1,2,3,4,100,1000,4354534543534
+		 // floating data  = 1.2 
+		 // Char
+		 // boolean - true, false
+		
+		// Numeric Data 
+		
+		// Computer , byte
+		// byte = unit of memory =  1byte  = 8bit = max value 255
+		
+		// byte = 1 byte ,    (2^8 - 1)  ,  -128 to 127 ,(-2^7 to (2^7)-1)
+		// short = 2 byte,    (2^16 -1)  , (-2^15 to (2^15)-1)
+		// int  = 4 byte,     (2^32 -1)  , (-2^31 to (2^31)-1)
+		// long = 8 byte      (2^64 -1)  , (-2^63 to (2^63)-1)
+		
+//		cyclic nature
+		
+		// 127  = 127
+		// 128 = -128
+		// 129 = -127
+//		byte b1 = (byte)130;
+//		System.out.println(b1);
+		
+//		5 = number (decimal number system)
+//		litre -  unit
+		
+		// 341 = 300 + 40 + 1
+		// 14536 = 10000 + 4000 + 500 + 30 + 6  = 
+		
+		// 12, 45, 23 , 78, 100
+		// binary, octal , hexadecimal
+		
+		
+		// binary
+		int i1 = 76;
+		int i2 = 076;
+		int i3 = 0X76; // octal , 0X76
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+		
+		
+		// Floating 
+//		float i = 1.34f; // 4 byte 
+//		double d = 1.56d; // 8 byte
+//		
+//		int k1 = 1;
+//		int k2 = 1;
+//		
+//		System.out.println(k1/k2);
+//		float v1 = 3.4f;
+//		float v2 = 3.4f;
+//		
+//		System.out.println(v1/v2);
+		
+		// characters
+		// my name is vaibhav
+		// ASCII Code - 1 byte char - 
+		// Unicode - 2 byte
+		
+//		char c1 = 'W';
+//		int c1Code = (int)c1;
+//		System.out.println(c1);
+//		System.out.println(c1Code);
+		
+//		int m = 116;
+//		char m1Char = (char)m;
+//		System.out.println(m1Char);
+		
+//		char j = '\u0916';
+//		System.out.println(j);
+		
+		
+		// Boolean , true/ false
+//		boolean b1 = false;
+		
+		
+		
+		
+		
+		
+		
+	}
+}
